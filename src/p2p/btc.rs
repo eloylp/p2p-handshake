fd2340635112bf0b4ae182ec9fc1a613e0818775
eloylp <eloylp@eloylp.dev>
2022-11-27 00:23:12 +0100

    Refactor, clearly split read and writes TCP streams
    This is a refactor that prepares the terrain for the next step. Which is to use message passing for sending messages.

--- src/p2p/btc.rs
+++ src/p2p/btc.rs
@@ -17,26 +17,31 @@ use bitcoin::{
 use bytes::{Buf, BytesMut};
 use tokio::{
     io::{self, AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
+    net::{
+        tcp::{ReadHalf, WriteHalf},
+        TcpStream,
+    },
 };
 
 use super::{Event, EventChain, EventDirection, HandshakeConfig};
 
 pub async fn handshake(config: HandshakeConfig) -> Result<EventChain, Box<dyn Error>> {
-    let stream = TcpStream::connect(&config.node_socket).await?;
+    
+    let mut stream = TcpStream::connect(&config.node_socket).await?;
+    let (mut recv_stream, mut write_stream) = stream.split();
 
-    let mut conn = Connection::new(stream, 1024);
+    let mut frame_reader = FrameReader::new(&mut recv_stream, 1024);
 
     let mut event_chain = EventChain::new();
     let node_socket = SocketAddr::from_str(&config.node_socket).unwrap();
     let version_message = version_message(&node_socket);
 
-    conn.write_message(&version_message).await?;
+    write_message(&mut write_stream, &version_message).await?;
     event_chain.add(Event::new("VERSION".to_string(), EventDirection::OUT));
 
     loop {
-        if let Some(message) = conn.read_message().await? {
-            handle_message(message, &mut conn, &mut event_chain).await?;
+        if let Some(message) = frame_reader.read_message().await? {
+            handle_message(message, &mut write_stream, &mut event_chain).await?;
         }
 
         if event_chain.len() == 4 {
@@ -46,9 +51,9 @@ pub async fn handshake(config: HandshakeConfig) -> Result<EventChain, Box<dyn Er
     return Ok(event_chain);
 }
 
-async fn handle_message(
+async fn handle_message<'a>(
     message: RawNetworkMessage,
-    conn: &mut Connection,
+    write_stream: &mut WriteHalf<'a>,
     event_chain: &mut EventChain,
 ) -> Result<(), Box<dyn Error>> {
     match message.payload {
@@ -63,7 +68,7 @@ async fn handle_message(
             event_chain.add(event);
             println!("{} {:?}", "received version!", v);
 
-            conn.write_message(&verack_message()).await?;
+            write_message(write_stream, &verack_message()).await?;
             let event = Event::new("ACK".to_string(), EventDirection::OUT);
             event_chain.add(event);
             Ok(())
@@ -108,19 +113,18 @@ pub fn version_message(dest_socket: &SocketAddr) -> RawNetworkMessage {
     }
 }
 
-struct Connection {
-    stream: TcpStream,
+struct FrameReader<'a> {
+    stream: &'a mut ReadHalf<'a>,
     buffer: BytesMut,
 }
 
-impl Connection {
-    pub fn new(stream: TcpStream, buff_size: usize) -> Connection {
-        Connection {
+impl FrameReader<'_> {
+    pub fn new<'a>(stream: &'a mut ReadHalf<'a>, buff_size: usize) -> FrameReader {
+        FrameReader {
             stream,
             buffer: BytesMut::with_capacity(buff_size),
         }
     }
-
     pub async fn read_message(&mut self) -> Result<Option<RawNetworkMessage>, Box<dyn Error>> {
         loop {
             if let Ok((message, count)) = deserialize_partial::<RawNetworkMessage>(&mut self.buffer)
@@ -138,10 +142,13 @@ impl Connection {
             }
         }
     }
+}
 
-    pub async fn write_message(&mut self, message: &RawNetworkMessage) -> io::Result<()> {
-        let data = serialize(message);
-        self.stream.write_all(data.as_slice()).await?;
-        Ok(())
-    }
+async fn write_message<'a>(
+    stream: &mut WriteHalf<'a>,
+    message: &RawNetworkMessage,
+) -> io::Result<()> {
+    let data = serialize(message);
+    stream.write_all(data.as_slice()).await?;
+    Ok(())
 }
