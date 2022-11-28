3ccad86787a900295293b9ec826913a40bdb8adb
eloylp <eloylp@eloylp.dev>
2022-11-28 13:29:04 +0100

    Do main error handling more polite

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::process::exit;
+
 use clap::Parser;
 use p2p_handshake::p2p;
 
 #[tokio::main]
 async fn main() {
     let config = p2p::HandshakeConfig::parse();
-    p2p::handshake(config).await.unwrap();
+    if let Err(err) = p2p::handshake(config).await {
+        println!("error executing p2p handshake: {}", err);
+        exit(1)
+    }
 }
